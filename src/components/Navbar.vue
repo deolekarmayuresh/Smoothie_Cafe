ecef13fce504921238dd03d7d3f8e64a0dd868de
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-11 18:23:25 +0530

    smoothie cafe

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended blue">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">MD's Smoothies</span>
+                </router-link>
+                <a href="" class="btn-floating btn-large halfway-fab red">
+                    <router-link to="">
+                        <i class="material-icons">add</i>
+                    </router-link>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+    .navbar nav{
+        padding: 0 20px;
+    }
+</style>
