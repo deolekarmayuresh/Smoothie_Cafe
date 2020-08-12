f7497c9732352ca28fefaf347517dda0442278bf
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-12 15:54:33 +0530

    firebase initialization

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCPhLCZrMxMpapraqru3mOuU-459MNjv8A",
+    authDomain: "mdsmoothiecafe.firebaseapp.com",
+    databaseURL: "https://mdsmoothiecafe.firebaseio.com",
+    projectId: "mdsmoothiecafe",
+    storageBucket: "mdsmoothiecafe.appspot.com",
+    messagingSenderId: "313756556184",
+    appId: "1:313756556184:web:3077b753574d3066a969ed",
+    measurementId: "G-1HLBB7G9PH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
