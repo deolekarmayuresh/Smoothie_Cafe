48fe7e261e7d7e3c01b62602ada523b027155721
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-13 19:01:45 +0530

    edit smoothie

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="edit-smoothie container">
+        <h2>Edit Smoothie</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
