f1544744f0b77e3ea3c9ee647cb655b27401b689
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-13 21:17:11 +0530

    edit smoothie

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,17 +1,47 @@
 <template>
-    <div class="edit-smoothie container">
-        <h2>Edit Smoothie {{ this.$route.params.smoothie_slug }}</h2>
+    <div v-if="smoothie" class="edit-smoothie container">
+        <h2>Edit {{ smoothie.title }} Smoothie</h2>
+        <form @submit.prevent="EditSmoothie">
+           <div class="field title">
+               <label for="title">Smoothie Title: </label>
+               <input type="text" name="title" v-model="smoothie.title">
+           </div>
+           <div v-for="(ing, index) in smoothie.ingredients" :key="index" class="field ">
+               <label for="ingredient">Ingredient:</label>
+               <input type="text" name="ingredient" v-model="smoothie.ingredients[index]">
+               <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+           </div>
+           <div class="field add-ingredient">
+               <label for="add-ingredient">Edit an Ingredient: </label>
+               <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+           </div>
+           <div class="field center-align">
+               <p v-if="feedback" class="red-text">{{ feedback }}</p>
+               <button class="btn pink">Edit Smoothie</button>
+           </div>
+       </form>
     </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
     name: 'EditSmoothie',
     data() {
         return {
-            
+            smoothie: null
         }
     },
+    created() {
+        let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+        ref.get().then(snapshot => {
+            snapshot.forEach(doc => {
+                this.smoothie = doc.data()
+                this.smoothie.id = doc.id
+            });
+        })
+    }
 }
 </script>
 
