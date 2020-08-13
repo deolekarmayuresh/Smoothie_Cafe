f4b6d6d6019a74ff21f8f9c7876be9cbafa8da10
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-13 14:42:21 +0530

    add smoothie

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,5 +1,20 @@
 <template>
-   
+   <div class="add-smoothie container">
+       <h2 class="center-align purple-text">Add New Smoothie Reciepe</h2>
+       <form>
+           <div class="field title">
+               <label for="title">Smoothie Title: </label>
+               <input type="text" name="title" v-model="title">
+           </div>
+           <div class="field add-ingredient">
+               <label for="add-ingredient">Add an Ingredient: </label>
+               <input type="text" name="add-ingredient">
+           </div>
+           <div class="field center-align">
+               <button class="btn pink">Add Smoothie: </button>
+           </div>
+       </form>
+   </div>
 </template>
 
 <script>
